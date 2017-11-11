ba88bb23409b11fca45d888ab4a3d011cbe5538c
Vova0199 <vova0143@gmail.com>
2017-11-11 21:04:14 +0200

    Add action on FAB

--- E_Advertisement/app/src/main/java/com/example/a37_1/e_advertisement/Main_screen.java
+++ E_Advertisement/app/src/main/java/com/example/a37_1/e_advertisement/Main_screen.java
@@ -2,6 +2,7 @@ package com.example.a37_1.e_advertisement;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TableRow;
@@ -11,22 +12,33 @@ public class Main_screen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         TableRow news1;
-        TableRow news2;
-        TableRow news3;
+        FloatingActionButton addNews = null;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        addNews = (FloatingActionButton) findViewById(R.id.addNews);
+        addNews.setOnClickListener(addnew);
+
         news1 = (TableRow) findViewById(R.id.news1);
-        news1.setOnClickListener(buttonClickListener);
+        news1.setOnClickListener(viewContent);
 
         news1 = (TableRow) findViewById(R.id.news2);
-        news1.setOnClickListener(buttonClickListener);
+        news1.setOnClickListener(viewContent);
 
         news1 = (TableRow) findViewById(R.id.news3);
-        news1.setOnClickListener(buttonClickListener);
+        news1.setOnClickListener(viewContent);
     }
 
-    View.OnClickListener buttonClickListener = new View.OnClickListener() {
+    View.OnClickListener viewContent = new View.OnClickListener() {
+        @Override
+        public void onClick(View v){
+            Intent intent=new Intent(v.getContext(),DescriptionNewActivity.class);
+            startActivityForResult(intent,0);
+
+        }
+    };
+    View.OnClickListener addnew = new View.OnClickListener() {
         @Override
         public void onClick(View v){
             Intent intent=new Intent(v.getContext(),Admin_page.class);
@@ -34,6 +46,7 @@ public class Main_screen extends AppCompatActivity {
 
         }
     };
+
 
 }
 
