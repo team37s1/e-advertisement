be5ce02d5c60de42837531ec4de205fc01e51f90
Vova0199 <vova0143@gmail.com>
2018-03-21 19:12:51 +0200

    [^] add comments

--- NodeMCU/Test_firmware/Test_firmware.ino
+++ NodeMCU/Test_firmware/Test_firmware.ino
@@ -1,23 +1,26 @@
-#define LEVEL_COUNT 3
+#define LEVEL_COUNT 3   //кількість рівнів 
 
-int level1 = 5;
+//оголошення портів
+int level1 = 5; 
 int level2 = 4;
 int level3 = 0;
 int ledpin = 16;
 
 void setup() {
+// цикл, який ініціалізує порти на вхід, тобто рівні
   for(int i = 1; i <= LEVEL_COUNT; i++){
     pinMode("level" + i, INPUT_PULLUP);
   }
+
   pinMode(ledpin, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(9600);   //відкриває послідовний порт, для спілкування з компютером
 }
 
 void loop() {
-  if (digitalRead(level1) == 1){
-    if(digitalRead(level2) == 1){
-      if(digitalRead(level3) == 1){
-        digitalWrite(ledpin, HIGH); 
+  if (digitalRead(level1) == 1){    //умова першого -  мінімального рівня
+    if(digitalRead(level2) == 1){   //умова 2 ріня
+      if(digitalRead(level3) == 1){ //умова 3 рівня
+        digitalWrite(ledpin, HIGH); // якщо максимальний рівень, то загорається дефолтна лампочка на платі
       }
       else{
       Serial.print("Level 3");
@@ -30,5 +33,4 @@ void loop() {
   else{
     Serial.print("Level 1");
   }
-
 }
