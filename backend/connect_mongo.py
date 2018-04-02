04f744abd25c8196e51b9ce556b79295c119e899
Vova0199 <vova0143@gmail.com>
2018-04-02 14:13:47 +0300

    [^] code optimization
    Co-Authored-By: Bogdan <bogdan2499@users.noreply.github.com>

--- backend/connect_mongo.py
+++ backend/connect_mongo.py
@@ -1,14 +1,12 @@
-from  flask import Flask, jsonify, request
-from  flask_pymongo import PyMongo
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = '37_1'
 app.config['MONGO_URI'] = 'mongodb://admin:admin@ds135574.mlab.com:35574/37_1'
 
-
 mongo = PyMongo(app)
-
 
 
 @app.route('/')
@@ -16,132 +14,80 @@ def default():
     return 'Ви на головній'
 
 
-@app.route('/api/news', methods=['GET'])
+@app.route('/api/news', methods=['GET', 'POST'])
 def get_all_news():
     news = mongo.db.news
     output = []
+    if request.method == 'GET':
+        for q in news.find():
+            output.append({'title': q['title'],
+                           'content': q['content'],
+                           'area': q['area'],
+                           'category': q['category']})
 
-    for q in news.find():
-        output.append({'title' : q['title'],
-                       'content' : q['content'],
-                       'area': q['area'],
-                       'category': q['category']})
+    elif request.method == 'POST':
+        title = request.json['title']
+        content = request.json['content']
+        area = request.json['area']
+        category = request.json['category']
 
-    return jsonify( output)
+        news_id = news.insert({'title': title,
+                               'content': content,
+                               'area': area,
+                               'category': category, })
+        new_news = news.find_one({'_id': news_id})
+
+        output = {'title': new_news['title'],
+                  'content': new_news['content'],
+                  'area': new_news['area'],
+                  'category': new_news['category']}
+
+    return jsonify(output)
 
 
-@app.route('/api/news/gaz', methods=['GET'])
-def get_gaz():
-    gaz = mongo.db.news
+def get_category(category):
+    category_name = mongo.db.news
     result = []
-    for q in gaz.find({'category' : 'Газопостачання'}):
+    for q in category_name.find({'category': category}):
         result.append({'title': q['title'],
                        'content': q['content'],
                        'area': q['area'],
                        'category': q['category']})
+    return result
+
+
+@app.route('/api/news/<category>', methods=['GET'])
+def get_news(category):
+    if category == "storm":
+        result = get_category("Штормове попередження")
+
+    elif category == "gaz":
+        result = get_category("Газопостачання")
+
+    elif category == "voda":
+        result = get_category("Водопостачання")
+
+    elif category == "street":
+        result = get_category("Перекриття вулиць")
+
+    elif category == "electro":
+        result = get_category("Електроенергія")
 
     return jsonify(result)
 
-
-@app.route('/api/news/voda', methods=['GET'])
-def get_voda():
-    voda = mongo.db.news
-    result = []
-    for q in voda.find({'category' : 'Водопостачання'}):
-        result.append({'title': q['title'],
-                       'content': q['content'],
-                       'area': q['area'],
-                       'category': q['category']})
-
-    return jsonify(result)
-
-@app.route('/api/news/electro', methods=['GET'])
-def get_electro():
-    electro = mongo.db.news
-    result = []
-    for q in electro.find({'category' : 'Електроенергія'}):
-        result.append({'title': q['title'],
-                       'content': q['content'],
-                       'area': q['area'],
-                       'category': q['category']})
-
-    return jsonify(result)
-
-@app.route('/api/news/street', methods=['GET'])
-def get_street():
-    street = mongo.db.news
-    result = []
-    for q in street.find({'category' : 'Перекриття вулиць'}):
-        result.append({'title': q['title'],
-                       'content': q['content'],
-                       'area': q['area'],
-                       'category': q['category']})
-
-    return jsonify(result)
-
-
-@app.route('/api/news/storm', methods=['GET'])
-def get_storm():
-    storm = mongo.db.news
-    result = []
-    for q in storm.find({'category' : 'Штормові попередження'}):
-        result.append({'title': q['title'],
-                       'content': q['content'],
-                       'area': q['area'],
-                       'category': q['category']})
-
-    return jsonify(result)
-
-@app.route('/api/news/test', methods=['GET'])
-def test():
-    storm = mongo.db.news
-    result = []
-    for q in storm.find({'category' : 'Штормові попередження'}):
-        result.append({'title': q['title'],
-                       'content': q['content'],
-                       'area': q['area'],
-                       'category': q['category']})
-
-    return jsonify(result)
-
-@app.route('/api/news', methods=['POST'])
-def add_news():
-    news = mongo.db.news
-
-    title = request.json['title']
-    content = request.json['content']
-    area = request.json['area']
-    category = request.json['category']
-
-    news_id = news.insert({'title' :     title,
-                           'content' : content,
-                           'area' : area,
-                           'category' : category,})
-    new_news = news.find_one({'_id' : news_id})
-
-    output = {'title': new_news['title'],
-                   'content': new_news['content'],
-                   'area': new_news['area'],
-                   'category': new_news['category']}
-    return jsonify({'result': output})
 
 @app.route('/api/news/mcu', methods=['POST'])
 def add_level():
     news = mongo.db.news
 
     title = request.json['title']
-
-
-    news_id = news.insert({'title' :     title})
-    new_news = news.find_one({'_id' : news_id})
-
+    news_id = news.insert({'title': title})
+    new_news = news.find_one({'_id': news_id})
     output = {'title': new_news['title']}
-
     return jsonify({'result': output})
-
 
 
 if __name__ == '__main__':
     app.debug = True
 
-    app.run(host='192.168.0.101', port=8000)
+    app.run(host='172.16.85.180', port=8000)
