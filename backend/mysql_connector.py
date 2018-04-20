4e5e0f469381b45fa71f85adec42a4c6d7f38bf3
Vova0199 <vova0143@gmail.com>
2018-04-20 20:44:30 +0300

    [+] Add method to get news by category, send JSON data

--- backend/mysql_connector.py
+++ backend/mysql_connector.py
@@ -3,7 +3,6 @@ import mysql.connector
 import json
 
 conn = mysql.connector.connect(user = "root", password = "0000", host="localhost", port = "3307",  database = "37_1")
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,9 +16,7 @@ def signUp():
     surname = request.json['surname']
     login = request.json['login']
     password = request.json['password']
-
     cursor = conn.cursor()
-
     cursor.execute("INSERT INTO users ( name , surname, login , password)VALUES(%s,%s,%s, %s)",
                    (name,   surname, login, password))
     conn.commit()
@@ -56,18 +53,59 @@ def get_all_news():
                  "from news left join area on news.area_id = area.id " \
                  "join category on news.Category_id=category.id"
         cursor.execute(script)
-        news  = cursor.fetchall()
+        row_headers=[x[0] for x in cursor.description] #this will extract row headers
+        rv = cursor.fetchall()
+        json_data=[]
+        for result in rv:
+             json_data.append(dict(zip(row_headers, result)))
 
+        return json.dumps(json_data)
 
     elif request.method == 'POST':
-        pass
+        name = request.json['title']
+        surname = request.json['content']
+        login = request.json['area']
+        password = request.json['category']
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO news (title, description, area_id, Category_id)VALUES(%s,%s,%s, %s)",
+                       (name, surname, login, password))
+        conn.commit()
 
-    return jsonify(news)
+        return "Новину створено"
+
+@app.route('/api/news/<category>', methods=['GET'])
+def get_news(category):
+    if category == "voda":
+        result = get_category(1)
+
+    elif category == "gaz":
+        result = get_category(2)
+
+    elif category == "electro":
+        result = get_category(3)
+
+    elif category == "storm":
+        result = get_category(4)
+
+    elif category == "street":
+        result = get_category(5)
+
+    return result
 
 
-@app.route("/home")
-def home():
-    pass
+def get_category(category_id):
+    cursor = conn.cursor()
+    script = """select news.Title, news.description, category.name_of_category, area.name_of_area  from news 
+                left join area on news.area_id = area.id 
+                inner join category 
+                on news.Category_id=""" + str(category_id) +"  GROUP BY news.Category_id";
+    cursor.execute(script)
+    row_headers = [x[0] for x in cursor.description]  # this will extract row headers
+    rv = cursor.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+    return json.dumps(json_data)
 
 
 if __name__ == "__main__":
