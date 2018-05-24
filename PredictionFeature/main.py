ce23205c1d94222651dea962c1c6aa61382a3a59
bodyisnumb <goblimgus@gmail.com>
2018-05-24 22:43:39 +0300

    Prediction Features 0.3
    added comments

--- PredictionFeature/main.py
+++ PredictionFeature/main.py
@@ -9,6 +9,7 @@ import numpy
 ##################### PART1:READ AND TEST #####################
 
 
+# create a difference transform of the dataset
 def difference(dataset):
 	diff = list()
 	for i in range(1, len(dataset)):
@@ -16,7 +17,7 @@ def difference(dataset):
 		diff.append(value)
 	return numpy.array(diff)
 
-
+# Make a prediction give regression coefficients and lag obs
 def predict(coef, history):
 	yhat = coef[0]
 	for i in range(1, len(coef)):
@@ -24,19 +25,16 @@ def predict(coef, history):
 	return yhat
 
 series = Series.from_csv('leveldata.csv', header=0)
-
-
+# split dataset
 X = difference(series.values)
 size = int(len(X) * 0.66)
 train, test = X[0:size], X[size:]
-
-
+# train autoregression
 model = AR(train)
 model_fit = model.fit(maxlag=6, disp=False)
 window = model_fit.k_ar
 coef = model_fit.params
-
-
+# walk forward over time steps in test
 history = [train[i] for i in range(len(train))]
 predictions = list()
 for t in range(len(test)):
@@ -46,8 +44,7 @@ for t in range(len(test)):
 	history.append(obs)
 error = mean_squared_error(test, predictions)
 print('Test MSE: %.3f' % error)
-
-
+# plot
 pyplot.plot(test)
 pyplot.plot(predictions, color='red')
 pyplot.show()
@@ -56,33 +53,29 @@ pyplot.show()
 ##################### PART2:SAVE #####################
 
 
+# load dataset
 series = Series.from_csv('leveldata.csv', header=0)
 X = difference(series.values)
-
-
+# fit model
 model = AR(X)
 model_fit = model.fit(maxlag=6, disp=False)
-
-
+# save model to file
 model_fit.save('ar_model.pkl')
-
-
+# save the differenced dataset
 numpy.save('ar_data.npy', X)
-
-
+# save the last ob
 numpy.save('ar_obs.npy', [series.values[-1]])
 
 
 ##################### PART3:LOAD AND PREDICT #####################
 
 
+# load model
 model = ARResults.load('ar_model.pkl')
 data = numpy.load('ar_data.npy')
 last_ob = numpy.load('ar_obs.npy')
-
-
+# make prediction
 predictions = model.predict(start=len(data), end=len(data))
-
-
+# transform prediction
 yhat = predictions[0] + last_ob[0]
 print('Prediction: %f' % yhat)
